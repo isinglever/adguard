3a6a8e2723bda8ca039ca0f600180dafa9ec3e4c
isingle <35318953+isinglever@users.noreply.github.com>
2022-11-19 12:33:48 +0800

    Update file name

--- js/netnewswire.js
+++ js/netnewswire.js
@@ -9,16 +9,24 @@
 
 // $request.scheme, $request.method, $request.url, $request.path, $request.headers
 
-const pre = "https://pbs.twimg.com";
-const rear = ".jpg";
+// https://pbs.twimg.com/profile_images/1402420199878504452/VNa4mmSS_normal.jpg
+
+
 
 let url = $request.url;
 let path = $request.path;
 
-let mid = url.substr(21,44);
+let index1 = url.lastIndexOf('_'); // return the final _ index
+let index2 = path.lastIndexOf('_');
+let index3 = url.lastIndexOf('.');
 
-let modifiedPath = mid.concat(rear);
-let modifiedURL = pre.concat(mid, rear);
+let str1 = url.substr(0, index1);
+let str2 = url.substr(index3);
+let str3 = path.substr(0, index2);
+
+
+let modifiedPath = str3 + str2;
+let modifiedURL = str1 + str2;
 
 
 $done({url: modifiedURL, path: modifiedPath});
