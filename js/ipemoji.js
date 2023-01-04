864a3a8c6b0221a8837f0ea0dd4f286df588c7bd
isingle <35318953+isinglever@users.noreply.github.com>
2023-01-04 11:40:50 +0800

    Update ipemoji.js

--- js/ipemoji.js
+++ js/ipemoji.js
@@ -13,7 +13,7 @@ if ($response.statusCode != 200) {
   }
   function ISP_ValidCheck(para) {
     if(para == "Multacom Corporation") {
-      return "Multacom"
+      return "MC Corp"
     } else if (para == "Oracle Corporation") {
         return "Oracle Corp"
     } else if (para == "Amazon Technologies Inc."){
