1fb93ac4b3f8b5f33a29465ed528dd21f5bf91d0
isingle <35318953+isinglever@users.noreply.github.com>
2023-03-17 15:28:26 +0800

    Update elsa.js 🙄

--- js/elsa.js
+++ js/elsa.js
@@ -7,8 +7,10 @@ update time: 2023.03.17
 [rewrite_local]
 
 # elsa speak
-#^https:\/\/pool\.elsanow\.io\/user\/api\/v2\/purchase$ url script-response-body https://raw.githubusercontent.com/isinglever/Adguard/main/js/elsa.js
-#^https:\/\/pool\.elsanow\.io\/content\/api\/v1\/programs\/download.+  url request-header (\r\n)x-session-token:.+(\r\n) request-header $1x-session-token: F3S0w0bysBQFdbjtxpFurrFv2ItBBcBkVQxUddQW+9vjt2JXM751ksqq5GAWpkl+kk9nhig9BGh9JhYHQaokmendY6zLZDscHiRkZD2HrdJclKVCLordAARJhYIrf5C+5OSK6ax2TA45CKi8S09FEtYXN4noXO7gt42NT6WPIv6DKhdIwVxQuIAMLU5abmpMTDlyWeI4ulBWcOQbuZWWZg==
+
+^https:\/\/pool\.elsanow\.io\/user\/api\/v2\/purchase$ url script-response-body https://raw.githubusercontent.com/isinglever/Adguard/main/js/elsa.js
+
+^https:\/\/pool\.elsanow\.io\/content\/api\/v1\/programs\/download.+  url request-header (\r\n)x-session-token:.+(\r\n) request-header $1x-session-token: F3S0w0bysBQFdbjtxpFurrFv2ItBBcBkVQxUddQW+9vjt2JXM751ksqq5GAWpkl+kk9nhig9BGh9JhYHQaokmendY6zLZDscHiRkZD2HrdJclKVCLordAARJhYIrf5C+5OSK6ax2TA45CKi8S09FEtYXN4noXO7gt42NT6WPIv6DKhdIwVxQuIAMLU5abmpMTDlyWeI4ulBWcOQbuZWWZg==
 
 
 
