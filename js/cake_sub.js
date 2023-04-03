19635a19d6a54dc85f33a0906f8f39583aff12c8
isingle <35318953+isinglever@users.noreply.github.com>
2023-04-03 22:53:20 +0800

    Update cake_sub.js

--- js/cake_sub.js
+++ js/cake_sub.js
@@ -4,8 +4,10 @@ obj.data = {
     "status" : "FREE_TRIAL",
     "paymentScheduled" : true,
     "productId" : "me.mycake.plus.subscription.1year",
-    "freeTrialPeriod" : 15,
-    "date" : "2023-04-16"
+   // "freeTrialPeriod" : 15,
+   // "date" : "2023-04-16"
+    "freeTrialPeriod" : 233,
+    "date" : "2033-02-23"
   };
 
 $done({body: JSON.stringify(obj)});
