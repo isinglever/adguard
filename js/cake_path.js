62196e94b823e6ede84a7af912eb5e01d64e089a
Ori <isinglever@gmail.com>
2023-05-05 19:54:05 +0800

    modify

--- js/cake_path.js
+++ js/cake_path.js
@@ -2,12 +2,12 @@ let obj = JSON.parse($response.body);
 
 delete obj.extra.timeSaleProduct;
 
-for (i in obj.data) {
-    for (j in obj.data[i].steps) {
-        if (obj.data[i].steps[j].type == "AD") {
-            delete  obj.data[i].steps[j];
-        }
-    }
-}
+// for (i in obj.data) {
+//     for (j in obj.data[i].steps) {
+//         if (obj.data[i].steps[j].type == "AD") {
+//             delete  obj.data[i].steps[j];
+//         }
+//     }
+// }
 
 $done({body: JSON.stringify(obj)});
