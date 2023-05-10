a1e8264471d9ad30b49c485476a77b1cc7fdcc1f
isingle <35318953+isinglever@users.noreply.github.com>
2023-05-10 14:18:20 +0800

    Modify app version

--- js/cake_path.js
+++ js/cake_path.js
@@ -2,7 +2,7 @@
 // $request.scheme, $request.method, $request.url, $request.path, $request.headers
 
 var modifiedPath = $request.path;
-modifiedPath = modifiedPath.replace("&appVersion=5.4.0", "");
+modifiedPath = modifiedPath.replace("&appVersion=5.4.1", "");
 
 
 $done({path: modifiedPath});
