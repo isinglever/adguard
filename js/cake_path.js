f95a20355d1cb6f0dca7ef33adf2ed361fe716ef
isingle <isinglever@gmail.com>
2023-07-11 17:22:05 +0800

    Update cake_path.js

--- js/cake_path.js
+++ js/cake_path.js
@@ -2,7 +2,7 @@
 // $request.scheme, $request.method, $request.url, $request.path, $request.headers
 
 var modifiedPath = $request.path;
-modifiedPath = modifiedPath.replace("&appVersion=5.4.1", "");
+modifiedPath = modifiedPath.replace("&appVersion=5.5.0", "");
 
 
 $done({path: modifiedPath});
