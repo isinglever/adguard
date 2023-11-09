8e031466dfa1a82f42f7bc2ec015a9d99b528c14
Ori <isinglever@gmail.com>
2023-11-09 22:00:20 +0800

    modify revenuecat.js

--- js/revenuecat.js
+++ js/revenuecat.js
@@ -1,21 +1,30 @@
+var ua = $request.headers['User-Agent'] || $request.headers['user-agent'];
 let obj = JSON.parse($response.body);
-obj["subscriber"]["subscriptions"]= {
-      "com.elevateapp.elevate.lifetime_subscription": {
-        "is_sandbox": false,
-        "period_type": "active",
-        "billing_issues_detected_at": null,
-        "unsubscribe_detected_at": null,
-        "expires_date": "2099-12-01T03:51:32Z",
-        "original_purchase_date": "2019-10-31T02:51:33Z",
-        "purchase_date": "2019-10-31T02:51:32Z",
-        "store": "app_store"
-      }
-    };
+if (ua.indexOf('ChatGPTApp') != -1)
+{
+
+}
+else if (ua.indexOf('Elevate') != -1)
+{
+  obj["subscriber"]["subscriptions"]= {
+    "com.elevateapp.elevate.lifetime_subscription": {
+      "is_sandbox": false,
+      "period_type": "active",
+      "billing_issues_detected_at": null,
+      "unsubscribe_detected_at": null,
+      "expires_date": "2099-12-01T03:51:32Z",
+      "original_purchase_date": "2019-10-31T02:51:33Z",
+      "purchase_date": "2019-10-31T02:51:32Z",
+      "store": "app_store"
+    }
+  };
 obj["subscriber"]["entitlements"]= {
-      "pro": {
-        "expires_date": "2099-12-01T03:51:32Z",
-        "product_identifier": "com.elevateapp.elevate.lifetime_subscription",
-        "purchase_date": "2019-10-31T02:51:32Z"
-      }
-    };
+    "pro": {
+      "expires_date": "2099-12-01T03:51:32Z",
+      "product_identifier": "com.elevateapp.elevate.lifetime_subscription",
+      "purchase_date": "2019-10-31T02:51:32Z"
+    }
+  };
+}
+
 $done({body: JSON.stringify(obj)});
