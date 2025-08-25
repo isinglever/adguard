bf0d99c8dc09b946239cc0c2d6e6c8560dbb0f3e
isingle <isinglever@gmail.com>
2025-08-25 10:01:23 +0800

    update

--- js/lingvist/lingvist_services.js
+++ js/lingvist/lingvist_services.js
@@ -1,6 +1,6 @@
 let obj = JSON.parse($response.body);
 
-obj.services.expiration_ts = "2029-09-06T10:10:41Z";
+obj.services.subscription.expiration_ts = "2029-09-06T10:10:41Z";
 obj.services.next_billing_ts = "2029-09-06T10:10:41Z"
 obj.services.active_until_ts = "2029-09-06T10:11:28.159292Z";
 obj.services.is_active = true;
--- js/lingvist/lingvist_validate.js
+++ js/lingvist/lingvist_validate.js
@@ -4,6 +4,7 @@ const key = "com.lingvist.unlimited_12_months.v11.full_1md_ft";
 obj.data.attributes.apple_validation_result.transactions[0].expiresDate = "2029-09-06T10:10:41Z";
 obj.data.attributes.paid_access_levels.premium.is_lifetime = true;
 obj.data.attributes.paid_access_levels.premium.is_active = true;
+obj.data.attributes.paid_access_levels.premium.expires_at = "2029-09-06T10:10:41.000000+0000";
 obj.data.attributes.subscriptions[key].is_lifetime = true;
 obj.data.attributes.subscriptions[key].expires_at = "2029-09-06T10:10:41.000000+0000";
 obj.data.attributes.subscriptions[key].is_active = true;
