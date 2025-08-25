74f66c28f8acf9efb4770d8cfc1be78a79f3c6a3
isingle <isinglever@gmail.com>
2025-08-25 10:07:01 +0800

    update

--- js/lingvist/lingvist_services.js
+++ js/lingvist/lingvist_services.js
@@ -1,8 +1,8 @@
 let obj = JSON.parse($response.body);
 
-obj.services.subscription.expiration_ts = "2029-09-06T10:10:41Z";
-obj.services.next_billing_ts = "2029-09-06T10:10:41Z"
-obj.services.active_until_ts = "2029-09-06T10:11:28.159292Z";
-obj.services.is_active = true;
+obj.services[0].subscription.expiration_ts = "2029-09-06T10:10:41Z";
+obj.services[0].next_billing_ts = "2029-09-06T10:10:41Z"
+obj.services[0].active_until_ts = "2029-09-06T10:11:28.159292Z";
+obj.services[0].is_active = true;
 
 $done({body: JSON.stringify(obj)});
