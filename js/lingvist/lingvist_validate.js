82db155b785547ebcd7a969c087874a23ff4c56e
isingle <isinglever@gmail.com>
2025-08-25 09:27:12 +0800

    update

--- js/lingvist/lingvist_validate.js
+++ js/lingvist/lingvist_validate.js
@@ -1,5 +1,5 @@
 let obj = JSON.parse($response.body);
 
-obj.data.attributes.apple_validation_result.transactions.expiresDate = "2029-09-06T10:10:41Z";
+obj.data.attributes.apple_validation_result.transactions[1].expiresDate = "2029-09-06T10:10:41Z";
 
 $done({body: JSON.stringify(obj)});
