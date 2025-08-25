f819573a2f58b21cd316389591cac901d4392473
isingle <isinglever@gmail.com>
2025-08-25 09:53:47 +0800

    update

--- js/lingvist/lingvist_validate.js
+++ js/lingvist/lingvist_validate.js
@@ -1,11 +1,12 @@
 let obj = JSON.parse($response.body);
+const key = "com.lingvist.unlimited_12_months.v11.full_1md_ft";
 
 obj.data.attributes.apple_validation_result.transactions[0].expiresDate = "2029-09-06T10:10:41Z";
 obj.data.attributes.paid_access_levels.premium.is_lifetime = true;
 obj.data.attributes.paid_access_levels.premium.is_active = true;
-obj.data.attributes.subscriptions.com.lingvist.unlimited_12_months.v11.full_1md_ft.is_lifetime = true;
-obj.data.attributes.subscriptions.com.lingvist.unlimited_12_months.v11.full_1md_ft.expires_at = "2029-09-06T10:10:41.000000+0000";
-obj.data.attributes.subscriptions.com.lingvist.unlimited_12_months.v11.full_1md_ft.is_active = true;
+obj.data.attributes.subscriptions[key].is_lifetime = true;
+obj.data.attributes.subscriptions[key].expires_at = "2029-09-06T10:10:41.000000+0000";
+obj.data.attributes.subscriptions[key].is_active = true;
 
 
 $done({body: JSON.stringify(obj)});
