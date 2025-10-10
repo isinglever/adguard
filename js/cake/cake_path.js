ef4dd91e083a9503f29d0c3bcf7467107877cbbc
Jueliang <isinglever@gmail.com>
2025-10-11 00:08:48 +0200

    Refactor cake_path.js and cake_today.js to improve readability and maintainability

--- js/cake/cake_path.js
+++ js/cake/cake_path.js
@@ -1,60 +1,61 @@
+;(function () {
+  try {
+    if (typeof $response !== 'undefined' && $response && $response.body) {
+      var obj = JSON.parse($response.body || '{}');
 
-try {
-  if (typeof $response !== 'undefined' && $response && $response.body) {
-    var obj = JSON.parse($response.body || '{}');
+      function isAdNode(node) {
+        if (!node || typeof node !== 'object') {
+          return false;
+        }
 
-    function isAdNode(node) {
-      if (!node || typeof node !== 'object') {
+        var type = node.type;
+        if (typeof type === 'string' && type.toUpperCase() === 'AD') {
+          return true;
+        }
+
+        if (typeof node.stepTitle === 'string' && node.stepTitle.trim().toUpperCase() === 'AD') {
+          return true;
+        }
+
         return false;
       }
 
-      var type = node.type;
-      if (typeof type === 'string' && type.toUpperCase() === 'AD') {
-        return true;
-      }
-
-      if (typeof node.stepTitle === 'string' && node.stepTitle.trim().toUpperCase() === 'AD') {
-        return true;
-      }
-
-      return false;
-    }
-
-    function pruneAds(value) {
-      if (!value || typeof value !== 'object') {
-        return;
-      }
-
-      if (Array.isArray(value)) {
-        for (var i = value.length - 1; i >= 0; i -= 1) {
-          var item = value[i];
-          if (isAdNode(item)) {
-            value.splice(i, 1);
-          } else {
-            pruneAds(item);
-          }
+      function pruneAds(value) {
+        if (!value || typeof value !== 'object') {
+          return;
         }
-        return;
+
+        if (Array.isArray(value)) {
+          for (var i = value.length - 1; i >= 0; i -= 1) {
+            var item = value[i];
+            if (isAdNode(item)) {
+              value.splice(i, 1);
+            } else {
+              pruneAds(item);
+            }
+          }
+          return;
+        }
+
+        var keys = Object.keys(value);
+        for (var j = 0; j < keys.length; j += 1) {
+          pruneAds(value[keys[j]]);
+        }
       }
 
-      var keys = Object.keys(value);
-      for (var j = 0; j < keys.length; j += 1) {
-        pruneAds(value[keys[j]]);
-      }
+      pruneAds(obj);
+      $done({ body: JSON.stringify(obj) });
+      return;
     }
-
-    pruneAds(obj);
-    $done({ body: JSON.stringify(obj) });
+  } catch (e) {
+    console.log('cake path script error: ' + e);
+    if ($response && $response.body) {
+      $done({ body: $response.body });
+    } else {
+      $done({});
+    }
     return;
   }
-} catch (e) {
-  console.log('cake path script error: ' + e);
-  if ($response && $response.body) {
-    $done({ body: $response.body });
-  } else {
-    $done({});
-  }
-  return;
-}
 
-$done({});
+  $done({});
+})();
--- js/cake/cake_today.js
+++ js/cake/cake_today.js
@@ -1,77 +1,79 @@
-try {
-  if (!$response || !$response.body) {
-    $done({});
-    return;
-  }
-
-  var obj = JSON.parse($response.body || '{}');
-
-  function normalizeAccessFlags(value) {
-    if (!value || typeof value !== 'object') {
+;(function () {
+  try {
+    if (!$response || !$response.body) {
+      $done({});
       return;
     }
 
-    if (Array.isArray(value)) {
-      for (var i = 0; i < value.length; i += 1) {
-        normalizeAccessFlags(value[i]);
+    var obj = JSON.parse($response.body || '{}');
+
+    function normalizeAccessFlags(value) {
+      if (!value || typeof value !== 'object') {
+        return;
       }
-      return;
-    }
 
-    if (Object.prototype.hasOwnProperty.call(value, 'restrictedNow')) {
-      value.restrictedNow = false;
-    }
-
-    if (Object.prototype.hasOwnProperty.call(value, 'membershipOnly')) {
-      value.membershipOnly = false;
-    }
-
-    if (Object.prototype.hasOwnProperty.call(value, 'freeCoreQuiz')) {
-      value.freeCoreQuiz = true;
-    }
-
-    if (Object.prototype.hasOwnProperty.call(value, 'restrictedAfterFreeTrial')) {
-      value.restrictedAfterFreeTrial = false;
-    }
-
-    var keys = Object.keys(value);
-    for (var j = 0; j < keys.length; j += 1) {
-      normalizeAccessFlags(value[keys[j]]);
-    }
-  }
-
-  function pruneMembershipBanner(value) {
-    if (!value || typeof value !== 'object') {
-      return;
-    }
-
-    if (Array.isArray(value)) {
-      for (var i = value.length - 1; i >= 0; i -= 1) {
-        var item = value[i];
-        if (item && typeof item === 'object' && item.type === 'membershipTimeSaleHomeBanner') {
-          value.splice(i, 1);
-        } else {
-          pruneMembershipBanner(item);
+      if (Array.isArray(value)) {
+        for (var i = 0; i < value.length; i += 1) {
+          normalizeAccessFlags(value[i]);
         }
+        return;
       }
-      return;
+
+      if (Object.prototype.hasOwnProperty.call(value, 'restrictedNow')) {
+        value.restrictedNow = false;
+      }
+
+      if (Object.prototype.hasOwnProperty.call(value, 'membershipOnly')) {
+        value.membershipOnly = false;
+      }
+
+      if (Object.prototype.hasOwnProperty.call(value, 'freeCoreQuiz')) {
+        value.freeCoreQuiz = true;
+      }
+
+      if (Object.prototype.hasOwnProperty.call(value, 'restrictedAfterFreeTrial')) {
+        value.restrictedAfterFreeTrial = false;
+      }
+
+      var keys = Object.keys(value);
+      for (var j = 0; j < keys.length; j += 1) {
+        normalizeAccessFlags(value[keys[j]]);
+      }
     }
 
-    var keys = Object.keys(value);
-    for (var k = 0; k < keys.length; k += 1) {
-      pruneMembershipBanner(value[keys[k]]);
+    function pruneMembershipBanner(value) {
+      if (!value || typeof value !== 'object') {
+        return;
+      }
+
+      if (Array.isArray(value)) {
+        for (var i = value.length - 1; i >= 0; i -= 1) {
+          var item = value[i];
+          if (item && typeof item === 'object' && item.type === 'membershipTimeSaleHomeBanner') {
+            value.splice(i, 1);
+          } else {
+            pruneMembershipBanner(item);
+          }
+        }
+        return;
+      }
+
+      var keys = Object.keys(value);
+      for (var k = 0; k < keys.length; k += 1) {
+        pruneMembershipBanner(value[keys[k]]);
+      }
+    }
+
+    normalizeAccessFlags(obj);
+    pruneMembershipBanner(obj);
+
+    $done({ body: JSON.stringify(obj) });
+  } catch (e) {
+    console.log('cake services script error: ' + e);
+    if ($response && $response.body) {
+      $done({ body: $response.body });
+    } else {
+      $done({});
     }
   }
-
-  normalizeAccessFlags(obj);
-  pruneMembershipBanner(obj);
-
-  $done({ body: JSON.stringify(obj) });
-} catch (e) {
-  console.log('cake services script error: ' + e);
-  if ($response && $response.body) {
-    $done({ body: $response.body });
-  } else {
-    $done({});
-  }
-}
+})();
